1263e6d0e957439f2502a8206ca85b6b8d5a68ca
Peter Xu <peterx@redhat.com>
2016-04-19 15:54:54 +0800

    ioapic: register IOMMU IEC notifier for ioapic
    Let IOAPIC the first consumer of x86 IOMMU IEC invalidation
    notifiers. This is only used for split irqchip case, when vIOMMU
    receives IR invalidation requests, IOAPIC will be notified to update
    kernel irq routes. For simplicity, we just update all IOAPIC routes,
    even if the invalidated entries are not IOAPIC ones.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- hw/intc/ioapic.c
+++ hw/intc/ioapic.c
@@ -31,6 +31,7 @@
 #include "sysemu/kvm.h"
 #include "target-i386/cpu.h"
 #include "hw/i386/apic-msidef.h"
+#include "hw/i386/x86-iommu.h"
 
 //#define DEBUG_IOAPIC
 
@@ -196,6 +197,14 @@ static void ioapic_update_kvm_routes(IOAPICCommonState *s)
         kvm_irqchip_commit_routes(kvm_state);
     }
 #endif
+}
+
+static void ioapic_iec_notifier(void *private, bool global,
+                                uint32_t index, uint32_t mask)
+{
+    IOAPICCommonState *s = (IOAPICCommonState *)private;
+    /* For simplicity, we just update all the routes */
+    ioapic_update_kvm_routes(s);
 }
 
 void ioapic_eoi_broadcast(int vector)
@@ -364,6 +373,18 @@ static void ioapic_realize(DeviceState *dev, Error **errp)
     qdev_init_gpio_in(dev, ioapic_set_irq, IOAPIC_NUM_PINS);
 
     ioapics[ioapic_no] = s;
+
+#ifdef CONFIG_KVM
+    if (kvm_irqchip_is_split()) {
+        X86IOMMUState *iommu = x86_iommu_get_default();
+        if (iommu) {
+            /* Register this IOAPIC with IOMMU IEC notifier, so that
+             * when there are IR invalidates, we can be notified to
+             * update kernel IR cache. */
+            x86_iommu_iec_register_notifier(iommu, ioapic_iec_notifier, s);
+        }
+    }
+#endif
 }
 
 static void ioapic_class_init(ObjectClass *klass, void *data)
